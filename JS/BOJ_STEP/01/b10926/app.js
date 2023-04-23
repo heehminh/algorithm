c3ff9576026fb1fa80403e498ed745501ae4b10c
heehminh <mink141416@gmail.com>
2023-04-23 22:49:12 +0900

    chore: 폴더정리

--- JS/BOJ_STEP/01/b10926/app.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim();
-
-function solution(input) {
-    console.log(`${input}??!`);
-}
-
-solution(input);
--- JS/BOJ_STEP/06_string/b1152/app.js
+++
@@ -1,3 +0,0 @@
-const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim().split(" ");
-console.log(input[0] === "" ? 0 : input.length);
