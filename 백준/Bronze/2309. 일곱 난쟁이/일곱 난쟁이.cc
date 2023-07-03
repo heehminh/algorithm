d0850f504d530d9e7e0c6ca189c7bff5f89f7f5e
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2023-07-03 22:21:37 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[9];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    for(int i=0; i<9; i++) {
+        cin >> a[i];
+    }
+    sort(a, a+9);
+
+    do {
+        int sum = 0;
+        for (int i=0; i<7; i++) sum += a[i];  
+
+        if (sum==100) break;
+    } while(next_permutation(a, a+9));
+
+    for (int i=0; i<7; i++) cout << a[i] << "\n";
+    
+    return 0;
+}
