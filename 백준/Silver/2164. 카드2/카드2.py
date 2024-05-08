c9993a858fa335cdffb41624dd1a0dfd5aabe23f
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2024-05-08 22:28:15 +0900

    [Silver IV] Title: 카드2, Time: 236 ms, Memory: 50960 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,11 +1,16 @@
-from collections import deque
-q = deque()
-N = int(input())
-for i in range(1, N+1):
-    q.append(i)
+# 1번 카드가 제일 위에, N번 카드가 제일 아래에 
+# 제일 위에 있는 카드를 버림, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 
 
-while len(q)>1:
-    q.popleft()
-    q.append(q.popleft())
+from collections import deque 
+q = deque()
+
+N = int(input())
+
+for i in range(N):
+    q.append(i+1)
+
+while(len(q) != 1):
+    q.popleft() # 제일 위에 있는 카드 버림 
+    q.append(q.popleft()) # 제일 위에 있는 카드 제일 아래에 넣기 
 
 print(q.popleft())
