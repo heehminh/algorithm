95e5dd3e7e0f30ef472b1f91cd2cb2176acd14c4
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2024-05-08 22:32:53 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 112.51 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 27.9 MB, 시간: 179.22 ms
+메모리: 27.8 MB, 시간: 112.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 27일 01:56:16
+2024년 05월 08일 22:32:50
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,12 +1,10 @@
-# 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거
-
+# 연속적으로 나타나는 숫자 
 def solution(arr):
     answer = []
     
-    for item in arr:        
-        # (1) 아무 원소가 없거나 
-        # (2) 정답 배열의 이전값이 현재값과 같지 않을 때 
-        if len(answer) == 0 or answer[len(answer)-1] != item:
-            answer.append(item)
+    for i in arr:
+        # 아무 원소가 없거나, 배열의 이전 값이 현재 값과 같지 않거나
+        if len(answer) == 0 or answer[-1] != i:
+            answer.append(i)
     
     return answer
