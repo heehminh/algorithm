2035a82c8cc266c4f20afc52e11bbb6bd89eb1f0
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2024-07-04 19:45:39 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,35 +1,42 @@
+# 미로 탐색: 길찾기 (최단거리탐색, BFS)
 from collections import deque
-
-dy = (0, 1, 0, -1)
-dx = (1, 0, -1, 0)
 
 N, M = map(int, input().split())
 board = [input() for _ in range(N)]
 
-def is_valid_coord(y, x): # 범위를 벗어나지 않는 좌표인지 확인 
+# 채크 배열 
+chk = [[False] * (M) for _ in range(N)]
+
+# 움직일 수 있는 방향 
+dy = (0, 1, 0, -1)
+dx = (1, 0, -1, 0)
+
+# 범위 체크: 유효한 좌표인지
+def is_valid_coord(y, x):
     return 0 <= y < N and 0 <= x < M
 
 def bfs():
-    chk = [[False] * M for _ in range(N)]
+    # 초기값 체크 
     chk[0][0] = True 
+    
     dq = deque()
-    dq.append((0, 0, 1)) # y, x, 몇칸을 밟았는지 
+    dq.append((0, 0, 1)) # y, x, ans 
+    
     while dq:
-        y, x, d = dq.popleft() # 현재칸의 위치를 나타내는 좌표
+        y, x, d = dq.popleft()
         
         if y == N-1 and x == M-1:
-            return d
+            return d 
         
         nd = d + 1 
+        
         for k in range(4):
             ny = y + dy[k]
             nx = x + dx[k]
-            if is_valid_coord(ny, nx) and board[ny][nx] == '1' and chk[ny][nx] == False:
-                # 조건) 
-                # 1. ny, nx가 범위를 벗어나지 않는 좌표인지
-                # 2. board에 존재하는지
-                # 3. 방문하지 않았는지 
+            
+            # 범위 체크
+            if is_valid_coord(ny, nx) and board[ny][nx] == '1' and not chk[ny][nx]:
                 chk[ny][nx] = True 
                 dq.append((ny, nx, nd))
 
-print(bfs())
+print(bfs())            
