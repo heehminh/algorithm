bef554f70ab8db569cf48a9a480dd4cf05f5e71f
heehminh <mink141416@gmail.com>
2024-07-06 00:31:53 +0900

    [Silver II] Title: 배열 돌리기, Time: 3560 ms, Memory: 51276 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
@@ -1,44 +1,49 @@
-from copy import deepcopy  
-  
-T = int(input())  
-for tc in range(T):  
-    N, rotate = map(int, input().split())  
-    arr = [list(map(int, input().split())) for _ in range(N)]  
-    # 결과 저장할 배열  
-    result = [[0]*N for _ in range(N)]  
-  
-    if rotate < 0:  
-        rotate = 360 + rotate  
-    # 제자리 일 경우 그대로 출력  
-    if rotate == 360 or rotate == 0:  
-        for l in arr:  
-            print(*l)  
-    else:  
-        # 45의 배수로 나오므로 나눈 몫만큼 반복해주어야 한다.  
-        for _ in range(rotate//45):  
-            for i in range(N):  
-                for j in range(N):  
-                    # 주 대각선이라면 가운데 행을 들고 와준다.  
-                    if i==j:  
-                        result[i][j] = arr[N//2][j]  
-                    # 가운데 행이라면 부 대각선을 들고와준다.  
-                    elif i == N//2:  
-                        result[i][j] = arr[N-j-1][j]  
-                    # 부 대각선이라면 가운데 열을 들고온다  
-                    # 부 대각선 좌표 방법 2가지                    
-                    # i+j = N-1                    
-                    # N-j-1 = i                    
-                    elif i+j == N-1:  
-                        result[i][j] = arr[i][N//2]  
-                    # 가운데 열이라면 주 대각선을 들고온다.  
-                    elif j == N//2:  
-                        result[i][j] = arr[i][i]  
-  
-                    # 모두 아니라면 같은 좌표값 들고오기  
-                    else:  
-                        result[i][j] = arr[i][j]  
-            # 회전을 더해야할수도 있으므로 복사해주기  
-            arr = deepcopy(result)  
-  
-        for k in result:  
-            print(*k)  
+# 배열 돌리기
+
+# 회전을 여러 번 해야하는 경우가 있으므로 deepcopy를 통해 복사 
+from copy import deepcopy
+
+T = int(input())
+for _ in range(T):
+    N, d = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    ans = [[0] * N for _ in range(N)]
+    
+    if d < 0:
+        # 반시계방향
+        # -45: 360+45로 처리하면 동일함
+        d += 360 
+    
+    if d == 360 or d == 0:
+        for i in board:
+            print(*i)
+            
+    else:        
+        for _ in range(d // 45):
+            mid = N//2 
+            
+            for i in range(N):
+                for j in range(N):
+                    # 1. 주 대각선 -> 가운데 열 
+                    if i == j:
+                        ans[i][j] = board[mid][j]
+                    
+                    # 2. 가운데 열 -> 부 대각선
+                    elif i == mid:
+                        ans[i][j] = board[N-j-1][j]
+                        
+                    # 3. 부 대각선 -> 가운데 행
+                    elif i+j == N-1:
+                        ans[i][j] = board[i][mid]
+                    
+                    # 4. 가운데 행 -> 주 대각선 
+                    elif j == mid:
+                        ans[i][j] = board[i][i]
+                    
+                    else:
+                        ans[i][j] = board[i][j]
+                        
+            board = deepcopy(ans)
+    
+        for k in ans:
+            print(*k)
