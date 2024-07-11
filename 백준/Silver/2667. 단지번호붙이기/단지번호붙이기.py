e4f03c0ec7a477b545cabc89ae78acd88967e2ab
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2024-07-11 23:56:26 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,35 +1,48 @@
-# 단지번호붙이기 
+# 단지번호붙이기
+# 연결요소의 개수(ans), 각 연결요소에 해당하는 개수(res)
+from collections import deque 
 
 N = int(input())
-board = [list(map(int, input())) for _ in range(N)]
+
+board = [list(input()) for _ in range(N)]
+
+def is_valid_coord(y, x):
+    return 0 <= y < N and 0 <= x < N
 
 dy = (0, 1, 0, -1)
 dx = (1, 0, -1, 0)
 
-def is_valid_board(y, x):
-    return 0<=y<N and 0<=x<N
-
-def dfs(y,x,d):
-    board[y][x] = 0
-    
-    for k in range(4):
-        ny = y + dy[k]
-        nx = x + dx[k]
-        if is_valid_board(ny, nx) and board[ny][nx] == 1:
-            d = dfs(ny, nx, d+1)
-                
-    return d 
-
-ans = 0 # 총 단지수       
+ans = 0 # 총 단지수 
 res = [] # 단지내 집의 수 
 
-for y in range(N):
-    for x in range(N):
-        if board[y][x] == 1:
+def bfs(x, y):
+    dq = deque()
+    dq.append((x, y))
+    d = 1 
+    
+    while dq:
+        x, y = dq.popleft()
+        
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            
+            if is_valid_coord(ny, nx) and board[nx][ny] == '1':
+                dq.append((nx, ny))
+                board[nx][ny] = '0'
+                d += 1 
+                
+    return d 
+    
+for i in range(N):
+    for j in range(N):
+        if board[i][j] == '1':
+            board[i][j] = '0'
+            num = bfs(i, j)
             ans += 1 
-            num = dfs(y, x, 1)
             res.append(num)
-       
-print(ans)  
-for i in sorted(res):
-    print(i)
+
+print(ans)
+
+for num in sorted(res):
+    print(num)
