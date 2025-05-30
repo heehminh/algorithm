0add5df03067ba1a2e397b81850bd84748538342
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2025-05-30 17:51:11 +0900

    [Bronze I] Title: ROT13, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.py
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -3,17 +3,21 @@ ans = ''
 
 for char in s:
     if char.isalpha():
-        if char.isupper():
-                if chr(ord(char)+13) <= 'Z':
-                    ans += chr(ord(char)+13)
-                else:
-                    ans += chr(ord(char)-13)
+        # 알파벳을 13만큼 밀고, 26으로 나눈 나머지를 더해서 원형 순환
+        base = ord('A') if char.isupper() else ord('a')
+        ans += chr((ord(char) - base + 13) % 26 + base)
+        
+        # if char.isupper():
+        #         if chr(ord(char)+13) <= 'Z':
+        #             ans += chr(ord(char)+13)
+        #         else:
+        #             ans += chr(ord(char)-13)
             
-        elif char.islower():
-            if chr(ord(char)+13) <= 'z':
-                ans += chr(ord(char)+13)
-            else:
-                ans += chr(ord(char)-13)
+        # elif char.islower():
+        #     if chr(ord(char)+13) <= 'z':
+        #         ans += chr(ord(char)+13)
+        #     else:
+        #         ans += chr(ord(char)-13)
     else:
         ans += char 
         
