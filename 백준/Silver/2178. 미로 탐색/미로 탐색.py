cf4e41809d6fc1e545c2424bbd70e60a6e923d66
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2025-06-03 09:45:28 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,42 +1,38 @@
-# 미로 탐색: 길찾기 (최단거리탐색, BFS)
-from collections import deque
+# 최단거리 BFS 
+import sys 
+from collections import deque 
+input = sys.stdin.readline
 
-N, M = map(int, input().split())
+N, M = map(int, input().split()) 
 board = [input() for _ in range(N)]
 
-# 채크 배열 
-chk = [[False] * (M) for _ in range(N)]
-
-# 움직일 수 있는 방향 
 dy = (0, 1, 0, -1)
 dx = (1, 0, -1, 0)
 
-# 범위 체크: 유효한 좌표인지
 def is_valid_coord(y, x):
-    return 0 <= y < N and 0 <= x < M
+    return 0 <= y <= N-1 and 0 <= x <= M-1
 
 def bfs():
-    # 초기값 체크 
-    chk[0][0] = True 
-    
+    chk = [[False] * M for _ in range(N)]
     dq = deque()
-    dq.append((0, 0, 1)) # y, x, ans 
+    chk[0][0] = True 
+    dq.append((0, 0, 1)) # y, x, d 
     
     while dq:
         y, x, d = dq.popleft()
         
+        # 종료조건 
         if y == N-1 and x == M-1:
             return d 
         
-        nd = d + 1 
+        nd = d+1 
         
         for k in range(4):
-            ny = y + dy[k]
-            nx = x + dx[k]
+            ny = dy[k] + y 
+            nx = dx[k] + x 
             
-            # 범위 체크
             if is_valid_coord(ny, nx) and board[ny][nx] == '1' and not chk[ny][nx]:
-                chk[ny][nx] = True 
+                chk[ny][nx] = 1 
                 dq.append((ny, nx, nd))
-
-print(bfs())            
+                
+print(bfs()) # d 
