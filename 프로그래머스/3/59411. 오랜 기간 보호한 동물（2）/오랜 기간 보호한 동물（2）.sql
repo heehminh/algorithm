54c806f99c62aeec86e250ffb84488f3a918c286
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2025-06-04 15:49:33 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT ins.ANIMAL_ID as ANIMAL_ID, ins.NAME as NAME
+from ANIMAL_INS as ins inner join ANIMAL_OUTS as outs on ins.ANIMAL_ID = outs.ANIMAL_ID
+order by datediff(outs.DATETIME, ins.DATETIME) desc 
+limit 2;
