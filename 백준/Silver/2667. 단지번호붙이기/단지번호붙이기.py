41f52c272007434a85edb7453790ca47baa00786
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2025-06-05 14:27:12 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,48 +1,41 @@
-# 단지번호붙이기
-# 연결요소의 개수(ans), 각 연결요소에 해당하는 개수(res)
-from collections import deque 
+# dfs
 
 N = int(input())
+board = [input() for _ in range(N)]
+res = []
 
-board = [list(input()) for _ in range(N)]
+chk = [[False]*N for _ in range(N)]
 
-def is_valid_coord(y, x):
+def is_valid_coord(y,x):
     return 0 <= y < N and 0 <= x < N
 
 dy = (0, 1, 0, -1)
 dx = (1, 0, -1, 0)
 
-ans = 0 # 총 단지수 
-res = [] # 단지내 집의 수 
-
-def bfs(x, y):
-    dq = deque()
-    dq.append((x, y))
-    d = 1 
+def dfs(y,x):
+    global cnt 
+    chk[y][x] = True 
     
-    while dq:
-        x, y = dq.popleft()
+    for k in range(4):
+        ny = y + dy[k]
+        nx = x + dx[k]
         
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            
-            if is_valid_coord(ny, nx) and board[nx][ny] == '1':
-                dq.append((nx, ny))
-                board[nx][ny] = '0'
-                d += 1 
-                
-    return d 
+        if is_valid_coord(ny,nx) and not chk[ny][nx] and board[ny][nx] == '1':
+            chk[ny][nx] = True 
+            res[cnt-1] += 1 
+            dfs(ny, nx)
     
-for i in range(N):
-    for j in range(N):
-        if board[i][j] == '1':
-            board[i][j] = '0'
-            num = bfs(i, j)
-            ans += 1 
-            res.append(num)
 
-print(ans)
+cnt = 0 
+for y in range(N):
+    for x in range(N):
+        if not chk[y][x] and board[y][x] == '1':
+            chk[y][x] = True 
+            cnt += 1 
+            res.append(1)
+            dfs(y,x)
 
-for num in sorted(res):
-    print(num)
+print(len(res))
+
+for row in sorted(res):
+    print(row)
