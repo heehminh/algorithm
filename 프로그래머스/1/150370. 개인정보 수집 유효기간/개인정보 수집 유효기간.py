ab9997df89633de71bb042d0d7a6253cf0377242
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2025-06-30 17:24:58 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.26 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,8 +1,11 @@
 def solution(today, terms, privacies):
     answer = []
     
-    today_year, today_month, today_day = map(int, today.split('.'))
-    today_to_date = today_year*12*28 + today_month*28 + today_day
+    def to_day(date):
+        year, month, day = map(int, date.split('.'))
+        return year*12*28 + month*28 + day 
+    
+    today_to_date = to_day(today)
     
     # term dict 작업 
     term_dict = {}
@@ -12,10 +15,9 @@ def solution(today, terms, privacies):
     
     for i in range(len(privacies)):
         first_date, term = privacies[i].split() # 2021.05.02, A
-        year, month, day = map(int, first_date.split('.')) # 2021, 5, 2 
-        due_date = year*12*28 + month*28 + day + term_dict[term]*28 - 1 
+        due_date = to_day(first_date) + term_dict[term]*28 - 1
 
-        if due_date < today_to_date: # 날짜 비교
+        if due_date < today_to_date: 
             answer.append(i+1) 
         
     return answer
