346d6023214ff3221ca9998a02d98e58d3bcf854
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2025-07-24 14:46:40 +0900

    [Silver II] Title: 유기농 배추, Time: 48 ms, Memory: 32776 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,55 +1,39 @@
-# 유기농 배추 
-# 연결요소의 개수 
+import sys 
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
-# 체크 배열 사용 X 
-# 방문한 노드는 방문 처리 
+dy = (0, 1, 0, -1)
+dx = (1, 0, -1, 0)
 
-from collections import deque
+def is_valid_coord(y,x):
+    return 0 <= y < N and 0 <= x < M 
+
+def dfs(y,x):
+    for k in range(4):
+        ny = y + dy[k]
+        nx = x + dx[k]
+        
+        if is_valid_coord(ny, nx) and board[ny][nx] == 1 and not chk[ny][nx]:
+            chk[ny][nx] = True 
+            dfs(ny, nx)
 
 T = int(input())
-
 for _ in range(T):
-
-    N, M, K = map(int, input().split()) # 가로, 세로, 배추의 개수
-
-    board = [[0] * N for _ in range(M)] # board[x][y]
-
+    M, N, K = map(int, input().split())
+    board = [[0]*M for _ in range(N)]
+    
+    ans = 0 
+    chk = [[False]*M for _ in range(N)]
+        
     for _ in range(K):
         x, y = map(int, input().split())
-        board[y][x] = 1
-        
-    ans = 0 
+        board[y][x] = 1 
 
-    dy = (0, 1, 0, -1)
-    dx = (1, 0, -1, 0)
-
-    def is_valid_coord(y, x):
-        return 0 <= y < M and 0 <= x < N
-
-    def bfs(y, x):
-        dq = deque()
-        dq.append((y, x))
-        
-        while dq:
-            y, x = dq.popleft()
-            
-            if y == M-1 and x == N-1:
-                return 
-            
-            for k in range(4):
-                ny = y + dy[k]
-                nx = x + dx[k]
-                
-                if is_valid_coord(ny, nx) and board[ny][nx] == 1:
-                    dq.append((ny, nx))
-                    # 칸 방문 처리 
-                    board[ny][nx] = 0 
-        return
-
-    for x in range(N):
-        for y in range(M):
-            if board[y][x] == 1:
-                bfs(y, x)
+    for y in range(N):
+        for x in range(M):
+            if not chk[y][x] and board[y][x] == 1:
+                chk[y][x] = True 
                 ans += 1 
+                dfs(y, x)
                 
     print(ans)
