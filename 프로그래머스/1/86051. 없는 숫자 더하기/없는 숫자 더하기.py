c2198b27a95fc701f8bcce03c1321ff4dbc5babd
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2025-10-16 18:04:35 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,8 +1,2 @@
-def solution(numbers):
-    answer = 0
-    
-    for i in range(10):
-        if i not in numbers:
-            answer += i 
-    
-    return answer
+def solution(numbers):    
+    return 45 - sum(numbers)
