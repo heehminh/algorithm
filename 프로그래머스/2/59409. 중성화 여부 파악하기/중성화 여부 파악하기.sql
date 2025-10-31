ac0541140a07662692a4d790f6bc2dbe4aaf6764
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2025-10-31 21:16:23 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,7 +1,7 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME,
-    case when SEX_UPON_INTAKE like 'Neutered%' then 'O'
+select ANIMAL_ID, NAME, 
+CASE 
+    when SEX_UPON_INTAKE like 'Neutered%' then 'O'
     when SEX_UPON_INTAKE like 'Spayed%' then 'O'
     else 'X'
-    end as 중성화 
-from ANIMAL_INS 
+end as "중성화"
+from ANIMAL_INS
