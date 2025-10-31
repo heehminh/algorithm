2dbc9d2510062c994145353fb52036654371daee
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2025-11-01 08:55:31 +0900

    [level 2] Title: 더 맵게, Time: 1899.82 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -3,20 +3,19 @@ import heapq
 def solution(scoville, K):
     # 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
     
-    answer = 0
+    answer = 0 
     heapq.heapify(scoville)
     
     while True:
         if scoville[0] >= K:
             return answer 
         
-        if len(scoville)<2: 
+        if len(scoville) < 2:
             return -1 
         
         temp = heapq.heappop(scoville) + heapq.heappop(scoville)*2
         heapq.heappush(scoville, temp)
         answer += 1 
         
-    
     return answer 
     
