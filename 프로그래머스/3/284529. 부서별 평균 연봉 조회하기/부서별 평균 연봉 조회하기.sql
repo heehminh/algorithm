db33076397a195875b54d707bf4308bb8a6b93b7
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2025-11-01 09:33:14 +0900

    [level 3] Title: 부서별 평균 연봉 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
+++ 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
@@ -1,5 +1,4 @@
--- 코드를 작성해주세요
-select d.dept_id, d.dept_name_en, round(sum(e.sal) / count(d.dept_id),0) as avg_sal
-from hr_department as d join hr_employees as e on d.dept_id = e.dept_id
-group by d.dept_id
+select d.dept_id, d.dept_name_en, round(avg(e.sal)) as avg_sal
+from HR_DEPARTMENT as d join HR_EMPLOYEES as e on d.dept_id = e.dept_id
+group by d.dept_id 
 order by avg_sal desc
