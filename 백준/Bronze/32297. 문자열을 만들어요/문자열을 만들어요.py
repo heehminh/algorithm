d1d18488fab65a621f432012c0291742e7317df6
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-01-28 11:37:11 +0900

    [Bronze III] Title: 문자열을 만들어요, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/32297. 문자열을 만들어요/문자열을 만들어요.py
+++ 백준/Bronze/32297. 문자열을 만들어요/문자열을 만들어요.py
@@ -1,10 +1,3 @@
 N = int(input())
-string = input()
 
-ans = False 
-
-for i in range(N-3):
-    if string[i:i+4] == 'gori':
-        ans = True 
-        
-print('YES') if ans else print('NO') 
+print('YES') if 'gori' in input() else print('NO') 
