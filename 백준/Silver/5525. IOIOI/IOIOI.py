86e0deef91e8835f36120d36a0bf0587bf6efd29
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-01-31 10:27:18 +0900

    [Silver I] Title: IOIOI, Time: 312 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,14 +2,20 @@ N = int(input())
 M = int(input())
 S = input() 
 
-str = 'I'
-for i in range(N):
-    str += 'OI'
-
 ans = 0 
-for i in range(M-len(str)+1):
-    
-    if str == S[i:i+len(str)]:
-        ans += 1 
+i = 0
+cnt = 0 
+
+while i < M-2:
+    if S[i:i+3] == 'IOI':
+        cnt += 1 
+        i += 2 
+        if cnt >= N:
+            ans += 1 
+            cnt -= 1 # 겹치는 경우 처리
+
+    else:
+        cnt = 0 
+        i += 1 
 
 print(ans)
