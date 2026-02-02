b890c04aec34f0069012742201b6c4ff0fc7a086
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-02-02 22:20:43 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
-WHERE INTAKE_CONDITION = 'Sick'
-ORDER BY ANIMAL_ID; 
+select animal_id, name
+from animal_ins 
+where intake_condition = 'Sick'
+order by animal_id
