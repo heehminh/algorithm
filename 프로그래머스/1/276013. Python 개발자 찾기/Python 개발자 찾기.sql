862d4175e1df283cb271c5be055cb6ba2f972017
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-02-06 13:19:23 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,5 +1,4 @@
--- 코드를 작성해주세요
-select ID, EMAIL, FIRST_NAME, LAST_NAME 
-from DEVELOPER_INFOS 
-where SKILL_1 = 'Python' or SKILL_2 = 'Python' or SKILL_3 = 'Python'
-order by ID
+select id, email, first_name, last_name
+from developer_infos
+where skill_1 = 'Python' or skill_2 = 'Python' or skill_3 = 'Python'
+order by id 
