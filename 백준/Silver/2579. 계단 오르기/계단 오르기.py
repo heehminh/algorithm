cf167fe0223579b0affb3c3440bb983b6cca222a
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-02-10 14:55:30 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+if N == 1:
+    print(int(input()))
+    exit()
+
+arr = [int(input()) for _ in range(N)]
+
+dp = [0] * N
+dp[0] = arr[0]
+dp[1] = arr[0] + arr[1]
+
+for i in range(2, N):
+    dp[i] = max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i])
+    
+print(dp[N-1])
