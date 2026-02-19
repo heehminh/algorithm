191e472329c41d4386aa69c5f3d0319e2014d9e2
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-02-19 14:21:28 +0900

    [Silver IV] Title: 큐 2, Time: 1528 ms, Memory: 175072 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,25 @@
+import sys 
+from collections import deque
+
+dq = deque()
+N = int(input())
+for _ in range(N):
+    method = sys.stdin.readline().split()
+    
+    if method[0] == "push":
+        dq.append(method[1])
+        
+    elif method[0] == "pop":
+        print(dq.popleft() if dq else -1)
+        
+    elif method[0] == "size":
+        print(len(dq))
+        
+    elif method[0] == "empty":
+        print(0 if dq else 1)
+        
+    elif method[0] == "front":
+        print(dq[0] if dq else -1)
+        
+    elif method[0] == "back":
+        print(dq[-1] if dq else -1)
