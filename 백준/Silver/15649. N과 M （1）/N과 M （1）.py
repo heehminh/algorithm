b6bb9ff04a79648e0cf5ac1294c255bfbb5d51b5
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-03-06 15:06:35 +0900

    [Silver III] Title: N과 M (1), Time: 160 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,30 +1,19 @@
-# 백준 15649 N과 M (1) 백트래킹 
-# N, M (길이가 M인 수열)
-# 1부터 N까지의 자연수 중에서 중복없이 M개를 고른 수열 
-# 테스트케이스(1) 3, 1
-# 1
-# 2
-# 3 
-
-# 테스트케이스(2) 4, 2 
-# 1 2 
-# 1 3 
-# 1 4 
-# 2 1 
-# 2 3 
-# 2 4 
-# 콤비네이션으로 뽑으면 되는듯
-
-from itertools import permutations
-
 N, M = map(int, input().split())
+res = []
+chk = [False] * (N+1)
 
-# arr을 만들기
-arr = []
-for i in range(N):
-    arr.append(i+1)
-
-for combi in permutations(arr, M):
-    for i in combi:
-        print(i, end=" ")
-    print()
+def dfs():
+    if len(res) == M:
+        print(*res)
+        return 
+    
+    for nxt in range(1, N+1):
+        if not chk[nxt]:
+            chk[nxt] = True 
+            res.append(nxt)
+            dfs()
+            
+            res.pop() # 백 
+            chk[nxt] = False 
+    
+dfs()
