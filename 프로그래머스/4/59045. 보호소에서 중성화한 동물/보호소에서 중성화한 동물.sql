c9019422f4a1c667ded895fd7b52682c8f4e9003
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-03-28 02:00:29 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,7 +1,4 @@
--- 코드를 입력하세요
-SELECT ins.animal_id, ins.animal_type, ins.name
-from animal_ins as ins join animal_outs as outs 
-on ins.animal_id = outs.animal_id 
-where ins.sex_upon_intake like 'Intact%' and 
-(outs.sex_upon_outcome = 'Spayed Female' or outs.sex_upon_outcome = 'Neutered Male')
-order by ins.animal_id 
+select ins.animal_id, ins.animal_type, ins.name
+from animal_ins as ins join animal_outs as outs on ins.animal_id = outs.animal_id
+where ins.sex_upon_intake like 'Intact%' and (outs.sex_upon_outcome like 'Spayed%' or outs.sex_upon_outcome like 'Neutered%')
+order by ins.animal_id
