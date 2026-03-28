985c5b8e6c7432f61faed54442a95710de126af1
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-03-28 22:20:26 +0900

    [Silver IV] Title: 그래서 대회 이름 뭐로 하죠, Time: 64 ms, Memory: 35404 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/27466. 그래서 대회 이름 뭐로 하죠/그래서 대회 이름 뭐로 하죠.py
+++ 백준/Silver/27466. 그래서 대회 이름 뭐로 하죠/그래서 대회 이름 뭐로 하죠.py
@@ -1,24 +1,43 @@
-from itertools import combinations
 import sys
 input = sys.stdin.readline 
 
 N, M = map(int, input().split())
 str = list(input().rstrip())
-ans = ''
-isFound = False 
 
-if 'A' not in str:
-    print('NO')
-    exit()
-
-for c in combinations(str, M):
-    if c[-1] not in ['A', 'E', 'I', 'O', 'U'] and c[M-2] == 'A' and c[M-3] == 'A':
-        isFound = True 
-        ans = c
+def is_fail(idx):
+    if idx == -1:
+        print('NO')
+        exit()
+    
+# 1) 맨 뒤 (AEIOU가 아님)
+idx1 = -1 
+for i in range(N-1, -1, -1):
+    if str[i] not in 'AEIOU':
+        idx1 = i 
         break 
 
-if isFound:
-    print('YES')
-    print(''.join(ans))
-else:
+is_fail(idx1)
+
+# 2) 뒤에서 2, 3번째 (A임)
+idx2 = -1 
+for i in range(idx1-1, -1, -1):
+    if str[i] == 'A':
+        idx2 = i 
+        break 
+
+is_fail(idx2)
+
+idx3 = -1
+for i in range(idx2-1, -1, -1):
+    if str[i] == 'A':
+        idx3 = i 
+        break 
+
+is_fail(idx3)
+
+# 3) 앞에서 M-3개 채워야함
+if idx3 < M-3:
     print('NO')
+else:
+    print('YES')
+    print(''.join(str[:M-3]) + 'AA' + str[idx1])
