0219f6242a93e4da86251a8769431547930a1bda
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-03-31 12:09:07 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,4 @@
--- 코드를 입력하세요
-SELECT outs.ANIMAL_ID, outs.NAME 
-from ANIMAL_OUTS as outs left join ANIMAL_INS as ins 
-on outs.ANIMAL_ID = ins.ANIMAL_ID 
-where ins.ANIMAL_ID is null 
-order by  outs.ANIMAL_ID, outs.NAME 
+select outs.animal_id, outs.name
+from animal_ins as ins right join animal_outs as outs on ins.animal_id = outs.animal_id 
+where ins.animal_id is null 
+order by outs.animal_id, outs.name;
