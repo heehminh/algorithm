884750689d9f35251cdeb02258ec5895fe308c51
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-04-20 23:50:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.78 ms, Memory: 23.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 26.2 MB, 시간: 74.84 ms
+메모리: 23.7 MB, 시간: 78.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 01일 23:04:10
+2026년 04월 20일 23:50:45
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,8 +2,8 @@ def solution(participant, completion):
     participant.sort()
     completion.sort()
     
-    for p, c in zip(participant, completion):
-        if p != c:
-            return p
+    for idx in range(len(completion)):
+        if participant[idx] != completion[idx]:
+            return participant[idx]
         
-    return participant[-1] # 순회에서 발견하지 못하면 마지막 경우
+    return participant[-1]
