5c44f9d0e27635be7ba8ce6b55ef3a3e6c24f09c
heehminh <mink141416@gmail.com>
2026-04-21 14:41:13 +0900

    [난이도 3] 할부는 몇 개월로 해드릴까요

---
+++ solvesql/3/할부는 몇 개월로 해드릴까요/할부는 몇 개월로 해드릴까요.sql
@@ -0,0 +1,6 @@
+select payment_installments, count(distinct order_id) as order_count,
+       min(payment_value) as min_value, max(payment_value) as max_value,
+       sum(payment_value) / count(*) as avg_value
+from olist_order_payments_dataset
+where payment_type = 'credit_card'
+group by payment_installments;
