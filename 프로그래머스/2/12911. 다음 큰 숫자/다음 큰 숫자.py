dabd1a1e96313540bfad8ed0696da28d89fc4508
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-05-01 16:43:03 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -2,7 +2,7 @@ def solution(n):
     next_num = n+1
     
     while True:
-        if bin(n).count('1') == bin(next_num).count('1'):
+        if format(n, 'b').count('1') == format(next_num, 'b').count('1'):
             return next_num
         
         next_num += 1 
