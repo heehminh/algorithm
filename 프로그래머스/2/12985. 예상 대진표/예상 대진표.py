9a7bb1c1ca02a9df5e60ae9a657084ab9c0ac77b
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-05-01 18:58:14 +0900

    [level 2] Title: 예상 대진표, Time: 2885.87 ms, Memory: 912 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,20 @@
+import math 
+
+def solution(n,a,b):
+    answer = 0
+    dp = []
+    max_round = int(math.log(n, 2))
+    num = 2
+    
+    for r in range(1, max_round+1):
+        tmp_list = [r]
+        for idx in range(1, n+1, num):
+            tmp_list.append([i for i in range(idx,idx+num)])
+        dp.append(tmp_list)
+        num *= 2 
+    
+    for i in range(len(dp)):  
+        round = dp[i][0]
+        for num_list in dp[i][1:]:
+            if a in num_list and b in num_list:
+                return round 
