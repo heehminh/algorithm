cd80c6354922849bce07f15b468aadafc44d49d6
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-05-05 02:33:17 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,27 +1,26 @@
 def solution(n, lost, reserve):
-    answer = 0
-    
-    arr = [1] * n
+    arr = [1] * n 
     
     for l in lost:
         arr[l-1] -= 1 
     
     for r in reserve:
         arr[r-1] += 1 
-        
-    for i in range(n):
-        if i > 0 and arr[i] == 2:
-            if arr[i-1] == 0:
-                arr[i-1] += 1 
-                arr[i] -= 1 
-                    
-        elif i < n-1 and arr[i] == 2:
-            if arr[i+1] == 0:
-                arr[i+1] += 1
-                arr[i] -= 1 
-                
-    for c in arr:
-        if c > 0:
+    
+    
+    for idx in range(n):
+        if arr[idx] > 1:
+            if idx > 0 and arr[idx-1] == 0:
+                arr[idx-1] += 1 
+                arr[idx] -= 1 
+            
+            elif idx < n-1 and arr[idx+1] == 0:
+                arr[idx+1] += 1
+                arr[idx] -= 1 
+    
+    answer = 0
+    for num in arr:
+        if num > 0: 
             answer += 1 
     
-    return answer # 1이상의 개수 
+    return  answer 
