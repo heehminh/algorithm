7879daef3664e737cb2134dabb6ba474364ba10c
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-05-05 18:39:41 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,10 @@
+select e.emp_no, e.emp_name, 
+case when sum(score)/2  >= 96 then 'S' 
+when sum(score)/2  >= 90 then 'A' 
+when sum(score)/2 >= 80 then 'B' else 'C' end as grade,
+case when sum(score)/2  >= 96 then sal * 0.2  
+when sum(score)/2  >= 90 then sal * 0.15  
+when sum(score)/2 >= 80 then sal * 0.1 else 0 end as bonus
+from hr_employees as e join hr_grade as g on e.emp_no = g.emp_no 
+group by e.emp_no
+order by 1 
