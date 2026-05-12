ccfc7449081ad34e22104e433a059b4dda7cd917
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-05-12 13:21:57 +0900

    [level 1] Title: 모의고사, Time: 2.37 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,17 +1,28 @@
 def solution(answers):
-    scores =  [0, 0, 0]
+    
     p = [
         [1, 2, 3, 4, 5],
         [2, 1, 2, 3, 2, 4, 2, 5],
         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     ]
     
-    for idx in range(len(answers)):
-        for k in range(3):
-            if answers[idx] == p[k][idx % len(p[k])]:
-                scores[k] += 1
+    scores = [0, 0, 0]
     
-    max_score = max(scores)
+    for i in range(len(answers)):
+        if answers[i] == p[0][i%5]:
+            scores[0] += 1 
+        
+        if answers[i] == p[1][i%8]:
+            scores[1] += 1 
+        
+        if answers[i] == p[2][i%10]:
+            scores[2] += 1 
     
-    return [i+1 for i, s in enumerate(scores) if s == max_score]
+    answer = []
+    
+    for idx in range(3):
+        if scores[idx] == max(scores):
+            answer.append(idx+1)
+    
+    return answer 
     
