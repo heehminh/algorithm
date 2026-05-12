0d0d9c80ab127ead12bfe6bfd5d95c0fcd3d3fdd
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-05-12 13:41:21 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,4 +1,4 @@
-select ins.animal_id, ins.name 
-from animal_ins as ins join animal_outs as outs on ins.animal_id = outs.animal_id
-where ins.datetime > outs.datetime 
-order by ins.datetime 
+select i.animal_id, i.name 
+from animal_ins as i join animal_outs as o on i.animal_id = o.animal_id 
+where i.datetime > o.datetime 
+order by i.datetime 
