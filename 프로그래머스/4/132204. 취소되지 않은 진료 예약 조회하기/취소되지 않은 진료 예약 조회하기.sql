03e561d79788933f491c5e3db0b1eb3620780aa0
Minhee Kim <76530562+heehminh@users.noreply.github.com>
2026-09-16 20:02:38 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,8 +1,4 @@
--- 코드를 입력하세요
-SELECT a.APNT_NO, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
-from APPOINTMENT as a join PATIENT as p on a.pt_no = p.pt_no
-join DOCTOR as d on a.MDDR_ID = d.dr_id
-where a.APNT_CNCL_YN = 'N' 
-and date_format(a.apnt_ymd, '%Y-%m-%d') = '2022-04-13'
-and a.mcdp_cd = 'CS'
-order by a.APNT_YMD 
+select a.apnt_no, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
+from patient as p join appointment as a on p.pt_no = a.pt_no join doctor as d on a.mddr_id = d.dr_id 
+where d.mcdp_cd = 'CS' and a.apnt_ymd like '2022-04-13%' and a.apnt_cncl_yn = 'N'
+order by a.apnt_ymd;
